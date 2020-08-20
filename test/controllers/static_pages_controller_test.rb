981861a42b601edd7a5880c53cc9387aee7e5ca6
Slaries <slaries@rambler.ru>
2020-08-20 22:52:53 +0300

    add new user controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -25,4 +25,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
+  test "should get signup" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Signup | Ruby on Rails Tutorial Sample App"
+  end
 end
